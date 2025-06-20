a9727624daf7ec11520f1f82fcb910fe610ec644
tajoad <tajoad@gmail.com>
2025-06-20 13:55:20 +0100

    updated changes to Number Datatypes

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +12,34 @@ namespace MyFirstProject
         static void Main(string[] args)
         {
 
-            int x = 10,
-                y = 20,
-                z = 30;
-
-            int age = 23;
-
+            string textAge = "-23";
+            int age = Convert.ToInt32(textAge);
             Console.WriteLine(age);
+
+            string textBigNumber = "-90000000";
+            long bigNumber = Convert.ToInt64(textBigNumber);
+            Console.WriteLine(bigNumber);
+
+            String textPrecison = "5.0000001";
+            float precision = Convert.ToSingle(textPrecison);
+            Console.WriteLine(precision);
+
+            string textMoney = "14.99";
+            decimal money = Convert.ToDecimal(textMoney);
+            Console.WriteLine(money);
+
+
+            //int x = 10,
+            //    y = 20,
+            //    z = 30;
+
+            //int age = 23;
+
+            //Console.WriteLine(age);
 
             Console.WriteLine(int.MaxValue);
             Console.WriteLine(int.MinValue);
-            long bigNumber = 900000000L;
+            long numBigNumber = 900000000L;
             Console.WriteLine(bigNumber);
 
             Console.WriteLine(long.MaxValue);
@@ -31,12 +49,12 @@ namespace MyFirstProject
             Console.WriteLine(double.MinValue);
             Console.WriteLine(double.MaxValue);
 
-            float precision = 5.000001F;
+            float numPrecision = 5.000001F;
             Console.WriteLine(float.MaxValue);
             Console.WriteLine(float.MinValue);
 
-            decimal money = 14.99M;
-            Console.WriteLine(money);
+            decimal numMoney = 14.99M;
+            Console.WriteLine(numMoney);
             Console.WriteLine(decimal.MaxValue);
             Console.WriteLine(decimal.MinValue);
 

72b0111373886fd47803ebacc5d515d67ddbc430
tajoad <tajoad@gmail.com>
2025-06-20 13:58:08 +0100

    updated master

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -10,37 +10,6 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-
-            int x = 10,
-                y = 20,
-                z = 30;
-
-            int age = 23;
-
-            Console.WriteLine(age);
-
-            Console.WriteLine(int.MaxValue);
-            Console.WriteLine(int.MinValue);
-            long bigNumber = 900000000L;
-            Console.WriteLine(bigNumber);
-
-            Console.WriteLine(long.MaxValue);
-            Console.WriteLine(long.MinValue);
-
-            double negative = -55.20;
-            Console.WriteLine(double.MinValue);
-            Console.WriteLine(double.MaxValue);
-
-            float precision = 5.000001F;
-            Console.WriteLine(float.MaxValue);
-            Console.WriteLine(float.MinValue);
-
-            decimal money = 14.99M;
-            Console.WriteLine(money);
-            Console.WriteLine(decimal.MaxValue);
-            Console.WriteLine(decimal.MinValue);
-
-
             Console.ReadLine();
         }
     }

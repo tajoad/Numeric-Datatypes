4f2f7b83b7c9d5c8dc38fabaa6398a75a86fcfb5
tajoad <tajoad@gmail.com>
2025-06-20 14:01:46 +0100

    Boolean Datatypes

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -10,36 +10,16 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
+            bool value = true;
 
-            int x = 10,
-                y = 20,
-                z = 30;
+            bool isMale = true;
 
-            int age = 23;
+            Console.WriteLine(value);
+            Console.WriteLine(isMale);
 
-            Console.WriteLine(age);
+            isMale = false;
 
-            Console.WriteLine(int.MaxValue);
-            Console.WriteLine(int.MinValue);
-            long bigNumber = 900000000L;
-            Console.WriteLine(bigNumber);
-
-            Console.WriteLine(long.MaxValue);
-            Console.WriteLine(long.MinValue);
-
-            double negative = -55.20;
-            Console.WriteLine(double.MinValue);
-            Console.WriteLine(double.MaxValue);
-
-            float precision = 5.000001F;
-            Console.WriteLine(float.MaxValue);
-            Console.WriteLine(float.MinValue);
-
-            decimal money = 14.99M;
-            Console.WriteLine(money);
-            Console.WriteLine(decimal.MaxValue);
-            Console.WriteLine(decimal.MinValue);
-
+            Console.WriteLine(isMale);
 
             Console.ReadLine();
         }
